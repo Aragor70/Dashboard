24cd41391c1cde8eaff761bff88c9cb75f130b1f
Aragor70 <mikey.prus@gmail.com>
2021-05-25 14:39:19 +0000

    Update: Server Connect database and initialize routes

---
+++ routes/api/resources.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const router = express.Router();
+
+
+
+router.get('/', (async( req, res ) => {
+    try {
+
+
+
+    } catch (err) {
+
+        console.log(err.message);
+
+    }
+
+}));
+
+module.exports = router;
+
--- server.js
+++ server.js
@@ -1,17 +1,25 @@
+require('dotenv').config({ path: 'config/config.env' })
 const express = require('express');
+const connect = require('./config/connect');
 const app = express();
+
+connect();
 
 app.use(express.json());
 
 
+app.use('/api/resources', require('./routes/api/resources'));
 
 
 
 
 
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server is running on port: ${PORT}.`));
 
 
-
-const PORT = 5000;
-
-app.listen(PORT, () => console.log(`Server is running on port: ${PORT}.`))
+process.on('unhandledRejection', (err, _promise) => {
+    console.error(`Error message: ${err.message}`)
+    server.close(() => process.exit(1))
+})
